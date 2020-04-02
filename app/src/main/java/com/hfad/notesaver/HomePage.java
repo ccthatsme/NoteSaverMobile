103194942c7693018faf02492726dbd87a4221a4
Chris <cciric44@gmail.com>
2020-04-01 22:41:06 -0400

    added the listener for clicking on the list entries

--- app/src/main/java/com/hfad/notesaver/HomePage.java
+++ app/src/main/java/com/hfad/notesaver/HomePage.java
@@ -3,7 +3,10 @@ package com.hfad.notesaver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -22,7 +25,24 @@ public class HomePage extends Activity {
 
         list.setAdapter(itemAdapter);
 
-        //need to finish this method
-       // list.setOnItemClickListener();
+
+       list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               Intent intent;
+
+               if(position == 0){
+                   intent = new Intent(HomePage.this, AddNote.class);
+                   startActivity(intent);
+               }
+
+               if(position == 1){
+                   intent = new Intent(HomePage.this, ViewAllNotes.class);
+                   startActivity(intent);
+               }
+
+
+           }
+       });
     }
 }
---
+++ app/src/main/java/com/hfad/notesaver/ViewAllNotes.java
@@ -0,0 +1,14 @@
+package com.hfad.notesaver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewAllNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all_notes);
+    }
+}
