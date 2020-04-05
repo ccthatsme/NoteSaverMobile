d2945543d888b30a7f48209b425ab03bc9e0ec87
Chris <cciric44@gmail.com>
2020-04-04 22:23:53 -0400

    added menu items to action bar

--- app/src/main/java/com/hfad/notesaver/AddNote.java
+++ app/src/main/java/com/hfad/notesaver/AddNote.java
@@ -3,8 +3,12 @@ package com.hfad.notesaver;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.ActionBar;
 
@@ -19,6 +23,25 @@ public class AddNote extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.view_note_action:
+                Intent intent = new Intent(this, ViewAllNotes.class);
+                startActivity(intent);
+                return true;
+
+             default:
+                 return super.onOptionsItemSelected(item);
+
+        }
     }
 }
--- app/src/main/java/com/hfad/notesaver/ViewAllNotes.java
+++ app/src/main/java/com/hfad/notesaver/ViewAllNotes.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ViewAllNotes extends AppCompatActivity {
 
@@ -16,5 +19,25 @@ public class ViewAllNotes extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main_for_view_all, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+
+            case R.id.create_note_action:
+                Intent intent2 = new Intent(this, AddNote.class);
+                startActivity(intent2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
